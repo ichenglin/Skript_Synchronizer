ca86dd2459572f0843c0d40a3937ba72ee2e72bf
fireclaws9 <41904540+fireclaws9@users.noreply.github.com>
2021-08-31 00:59:04 +0800

    Fixed File Uploader Date Display

--- src/modules/dateDisplay.js
+++ src/modules/dateDisplay.js
@@ -14,16 +14,16 @@ function dateDisplay(dateMilliseconds, dateFormat) {
     };
 
     return dateFormat.replace(/(Y+|M+|D+|H+|m+|s+)/g, (requestPlaceholder) => (
-        dateDisplayPlaceholders[requestPlaceholder[0]] ?
+        typeof dateDisplayPlaceholders[requestPlaceholder[0]] === "number" ?
         fitNumberToTextLength(dateDisplayPlaceholders[requestPlaceholder[0]], requestPlaceholder.length) :
-        requestPlaceholder
+        requestPlaceholder + "<" + dateDisplayPlaceholders.H
     ));
 
 }
 
 function fitNumberToTextLength(integerNumber, length) {
 
-    const numberTextLength = (Math.floor(Math.log10(integerNumber)) + 1);
+    const numberTextLength = (integerNumber === 0 ? 1 : (Math.floor(Math.log10(integerNumber)) + 1));
 
     if (numberTextLength < length) {
         return "0".repeat(length - numberTextLength) + integerNumber.toString();
